a7ff4763f2d50e595eaffa513cbb77912fa1a474
Amna <naheedamna5@gmail.com>
2024-12-30 12:09:48 +0500

    dates/Array part1

--- lecture5/date.js
+++ lecture5/date.js
@@ -9,6 +9,27 @@ const mydate= new Date();
 
 // let createdDate=new Date("2023-12-5");
 let createdDate=new Date(2023,3,0); // MM/DD/YYY
-console.log(createdDate.toString());
+// console.log(createdDate.toString());
 
 
+// let hello7= [0,5,8,1,4,-3,1,-6];
+ 
+// console.log(Math.min(...hello7));
+// console.log(Math.max(...hello7));
+
+
+let timeStamp= Date.now();
+// console.log(createdDate.getTime()/1000);
+// console.log(createdDate.getMonth());
+// console.log(createdDate.getDay());
+// console.log(createdDate.getMinutes());
+// console.log(createdDate.getMilliseconds());
+
+const new1 = mydate.toLocaleString('default', {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+
+})
+console.log(new1);
---
+++ lecture6/array.js
@@ -0,0 +1,42 @@
+// let myArr= [1,3,4,6,8,"ahmad",true, 4.8];
+let numArr = [1, 2, 3, 4, 5, 6, 7]
+let stringArr = ['amna', 'ahamd', 'ali'];
+
+// console.log(myArr[6]);
+
+// numArr.push(12);
+// numArr.pop();
+// console.log(numArr);
+
+// numArr.unshift(89);
+// numArr.shift();
+//  console.log(numArr);
+//  console.log(numArr.includes(4));
+//  console.log(numArr.indexOf(1));
+let typeArrInJoin=numArr.join();
+// console.log(typeof typeArrInJoin);
+
+
+console.log("a",numArr);
+let sliceArry= numArr.slice(1,6);
+// console.log("b",sliceArry);
+// console.log("a",numArr);
+
+let spliceArry= numArr.splice(1,6);
+console.log("c",spliceArry);
+console.log("a",numArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
