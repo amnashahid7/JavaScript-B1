1b2d7bdd17ec325422989491b772bf66d75ee8cf
Amna <naheedamna5@gmail.com>
2025-01-10 11:56:27 +0500

    object destructure & Json api

---
+++ lecture8/obj-de.js
@@ -0,0 +1,30 @@
+
+// Object Destructoring
+
+const cousre ={
+    courseName:"Javascript",
+    cousreId:2,
+    courseDuration:"2 month"
+}
+
+// console.log(cousre.courseName);
+
+// const {courseName,cousreId,courseDuration}= cousre;
+// console.log(courseName,courseDuration,cousreId);
+
+
+const {courseName:cName}= cousre;
+console.log(cName);
+
+// const nav=({company})={
+
+// }
+
+
+// {
+//     "name":"amna",
+//     "id":2,
+// }
+
+
+
