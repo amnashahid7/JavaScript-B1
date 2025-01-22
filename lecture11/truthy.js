b62c946dcf24df96c30eb2f3e4957a3dd68b6b8b
Amna <naheedamna5@gmail.com>
2025-01-22 13:14:36 +0500

    for loop/ truthy and falsy values

---
+++ lecture11/truthy.js
@@ -0,0 +1,80 @@
+let grade = 12;
+let range = Math.floor(grade / 10);
+// console.log(range);
+// switch (range) {
+//     case 10: // This accounts for perfect scores like 100
+//     case 9:
+//         console.log("A");
+//         break;
+//     case 8:
+//         console.log("B");
+//         break;
+//     case 7:
+//         console.log("C");
+//         break;
+//     case 6:
+//         console.log("D");
+//         break;
+//     default:
+//         console.log("F");
+//         break;
+// }
+
+
+
+// let email="ahmad@gmail.com";
+// if(email){
+// console.log(email);
+
+// }
+// else{
+//     console.log('please enter the email');
+    
+// }
+// let email1=[];
+// if(email1.length){
+// console.log(email1);
+
+// }
+// else{
+//     console.log('please enter the email');
+    
+// }
+let email2={};
+if(Object.keys(email2).length){
+console.log(email2);
+
+}
+else{
+    console.log('please enter the email11');
+    
+}
+
+
+// false value:
+// false, 0,-0 "",BigInt 0n,  null, undefined, NaN
+// truth values:
+// true, "0", [], "false", " ", {}, function (){}
+
+
+// Terniary Operator ?
+
+let price=95;
+
+//price >=90 ? console.log("greater then 90"): console.log("less then 90");
+
+
+
+
+// Nullish coalescing assignment/ operator ??
+ let val1= 5 ?? 10;
+ let val12= undefined ?? 100 ?? 200;
+ 
+
+console.log(val12);
+
+
+
+
+
+
---
+++ lecture12/forLoop.js
@@ -0,0 +1,11 @@
+// for (let index = 0; index <=10; index++) {
+//     const element = index;
+//     console.log(element);
+    
+// }
+
+let myArray=[1,2,3,4,556,6,66655,33,44]
+for (let i = 1; i < myArray.length; i++) {
+    console.log(myArray[i]);
+    
+}
