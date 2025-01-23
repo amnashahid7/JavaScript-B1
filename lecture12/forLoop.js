6b2062235004cc619eddeb11a4951df5951af9a1
Amna <naheedamna5@gmail.com>
2025-01-23 12:35:17 +0500

    for loop, while loop, do while loop

--- lecture12/forLoop.js
+++ lecture12/forLoop.js
@@ -4,8 +4,37 @@
     
 // }
 
-let myArray=[1,2,3,4,556,6,66655,33,44]
-for (let i = 1; i < myArray.length; i++) {
-    console.log(myArray[i]);
+// let myArray=[1,2,3,4,556,6,66655,33,44]
+// for (let i = 1; i < myArray.length; i++) {
+//     console.log(myArray[i]);
+    
+// }
+
+// for (let i = 1; i <=10; i++) {
+//     for (let j = 1; j <=10; j++) {
+//         console.log(`${i} * ${j} = ` + i*j);
+
+//         }
+    
+// }
+
+
+//break & continue
+
+
+// for (let i = 0; i <=10; i++) {
+//     if(i==5){
+//         console.log("number is 5");
+//         break
+//     }
+//     console.log(i);
+    
+// }
+for (let i = 0; i <=10; i++) {
+    if(i==5){
+        //console.log("number is 5");
+        continue;
+    }
+    console.log(i);
     
 }
---
+++ lecture12/whileLoop.js
@@ -0,0 +1,15 @@
+//while loop
+// let index=11;
+// while (index<=10) {
+//     console.log(index);
+//     index= index+2
+// }
+
+
+// do while loop
+// let i=11;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i<10);
+
