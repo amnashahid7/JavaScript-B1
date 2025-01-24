8dbf8d481bdd623a2a98fb12e52117b7cc048768
Amna <naheedamna5@gmail.com>
2025-01-24 12:12:12 +0500

    for of & for in Loop

---
+++ lecture12/forIn.js
@@ -0,0 +1,31 @@
+let myObj = {
+    'name': "ahamad",
+    'age': 10,
+}
+
+for (const key in myObj) {
+ // console.log(myObj[key]);
+  
+}
+
+// let myArr=[1,2,3,5,5,6,7,78];
+// for (const key in myArr) {
+//   console.log(myArr[key]);
+  
+// }
+
+let mystring="helooo";
+for (const key in mystring) {
+    //console.log(mystring[key]);
+    
+}
+
+
+let map = new Map();
+map.set('a',"ahamad");
+map.set('b', "amna");
+
+for (const key in map) {
+   console.log(key);
+   
+}
