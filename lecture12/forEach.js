ba7cd233589e6f7045b748cdd35d7d4a53527765
Amna <naheedamna5@gmail.com>
2025-01-28 13:04:46 +0500

    filter method

--- lecture12/forEach.js
+++ lecture12/forEach.js
@@ -1,7 +1,7 @@
-let coding=["js","py","c++", "c", "ruby","java"];
+let coding = ["js", "py", "c++", "c", "ruby", "java"];
 // coding.forEach(function name(item){
 //     console.log(item);
-    
+
 // })
 
 
@@ -10,45 +10,45 @@ let coding=["js","py","c++", "c", "ruby","java"];
 // coding.forEach((item)=>{
 //     if(coding[0]=='js'){
 //         console.log(item);
-    
+
 //     }
 //     else{
 //         console.log("item does not found");
 //         break;
 //     }
-    // })
+// })
 
 
 
 // coding.forEach( ()=>{})
 
 
-    // if(coding[0]=='js'){
-    //     coding.forEach((item)=>{
-    //         console.log(item);
-            
-    //     })
-    // }
-    // else{
-    //     console.log("item not found");
-        
-    // }
+// if(coding[0]=='js'){
+//     coding.forEach((item)=>{
+//         console.log(item);
+
+//     })
+// }
+// else{
+//     console.log("item not found");
+
+// }
 
 
 
 
-    // function printMe(num){
-    //     console.log(num);
-        
-    // }
+// function printMe(num){
+//     console.log(num);
 
-    // coding.forEach(printMe);
+// }
 
-
+// coding.forEach(printMe);
 
 
-    // ["","",""]
-    // [{},{},{}]
+
+
+// ["","",""]
+// [{},{},{}]
 
 
 
@@ -73,7 +73,62 @@ let coding=["js","py","c++", "c", "ruby","java"];
 // })
 
 
-coding.forEach((item, key, arr)=>{
-    console.log(item,key,arr);
-    
+coding.forEach((item, key, arr) => {
+    console.log(item, key, arr);
+
 })
+
+
+
+
+
+
+
+
+
+// Practice questions
+// //let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 7, 5, 3, 4, 7, 5, 6, 7, 3, 4, 5, 7, 5, 3, 6, 8]
+// let num = [1, 2, 3, 7, 7, 8, 8, 7, 7]
+// let count = 0;
+
+// let func3 = (number) => {
+//     if (number === check) {
+//         count++;
+//         console.log(count);
+
+//     }
+//     // if(!(number===check))
+//     // {
+//     //     return (`Value Not Matched in Array.`);
+//     // }
+// //     else {
+// //         console.log(`Value Not Matched in Array.`);
+// //     }
+// }
+// check = 7;
+// num.forEach(func3);
+
+// console.log(`${check} occurred ${count} time in the Array.`);
+
+
+
+
+let num = [1, 2, 3, 7, 7, 8, 8, 7, 7];
+let count = 0;
+let notMatchedPrinted = false; // Flag to ensure the else statement prints only once
+
+let func3 = (number) => {
+    if (number === check) {
+        count++;
+        console.log(count);
+    } else if (!notMatchedPrinted) {
+        console.log(`Value Not Matched in Array.`);
+        //notMatchedPrinted = true; // Set the flag to true so it won't print again
+    }
+}
+
+let check = 7;
+num.forEach(func3);
+
+console.log(`${check} occurred ${count} times in the Array.`);
+
