a3a5605ed39e72956d2720f25706bdb9d0b40d1e
Amna <naheedamna5@gmail.com>
2025-02-04 13:00:09 +0500

    Dom intro

--- lecture12/reduce.js
+++ lecture12/reduce.js
@@ -7,7 +7,7 @@ const myNums = [1, 2, 3]
 
 const myTotal = myNums.reduce((acc, curr) => acc + curr, 0)
 
-console.log(myTotal);
+//console.log(myTotal);
 
 
 const shoppingCart = [
@@ -31,5 +31,27 @@ const shoppingCart = [
 
 const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
 
-console.log(priceToPay);
-// test
+// console.log(priceToPay);
+
+
+
+const transactions = [
+    { category: "Food", amount: 20, quantity: 2 },
+    { category: "Transport", amount: 15, quantity: 1 },
+    { category: "Food", amount: 10, quantity: 1 },
+    { category: "Shopping", amount: 50, quantity: 3 },
+    { category: "Transport", amount: 20, quantity: 2 },
+    { category: "Food", amount: 15, quantity: 2 }
+];
+
+let totalTrans = transactions.reduce((acc, val) => {
+
+    if (!acc[val.category]) {
+        acc[val.category] = { amount: 0, quantity: 0 };
+    }
+    acc[val.category].amount += val.amount;
+    acc[val.category].quantity += val.quantity;
+    return acc
+}, {})
+
+console.log(totalTrans);
