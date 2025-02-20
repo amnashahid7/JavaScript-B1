ad3086dbe04377c18b36900738028ce081e25b93
Amna <naheedamna5@gmail.com>
2025-02-20 12:28:52 +0500

    clock and guessNumber game part 1

---
+++ SimpleProject/DigitalClock/index.js
@@ -0,0 +1,9 @@
+let clock = document.getElementById('banner');
+
+
+setInterval(function () {
+    let date = new Date();
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
+
